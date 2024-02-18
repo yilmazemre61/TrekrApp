f1a3fccb7f3d32b428ce8a07eaaf6ae28ecf69a9
emreyilmaz61 <emreyilmz1@gmail.com>
2024-02-18 15:23:39 -0500

    Initial Commit

---
+++ Trekr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trekr
+//
+//  Created by Emre Yilmaz on 2024-02-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Emre Yilmaz on 2024-02-18.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
